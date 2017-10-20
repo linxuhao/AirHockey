a7199b7ae680ecc3f48217adeb700ac775a2b755
Lin xuhao <xuhao.lin@etu.utc.fr>
2017-10-20 19:26:04 +0200

    first test of color tracking, result seems not promising!

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,6 +10,24 @@ public class PlayerController : MonoBehaviour {
     public float playZoneXEnd;
     public float playZoneZBegin;
     public float playZoneZEnd;
+    public GameObject player;
+
+    //ArToolKit functions
+    void OnMarkerFound(ARMarker marker)
+    {
+        Debug.Log("[PlayerController] - MARKER FOUND! YEAHH! --------------------------------------------------------------------------------");
+    }
+
+    void OnMarkerLost(ARMarker marker)
+    {
+        Debug.Log("[PlayerController] - MARKER LOST! FKKKKKK! --------------------------------------------------------------------------------");
+    }
+
+    void OnMarkerTracked(ARMarker marker)
+    {
+        //player.transform.position = marker.
+        Debug.Log("[PlayerController] - MARKER TRACKED! WHEEEE! --------------------------------------------------------------------------------");
+    }
 
     // Use this for initialization
     void Start () {
@@ -18,12 +36,12 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
-        Vector3 moussePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 10.0f));
-        //Debug.Log("[PlayerController] - mousse position x : " + moussePosition.x + ", mousse position Z : " + moussePosition.z);
-        if (mouseInDefinedPlayZone(moussePosition.x, moussePosition.z))
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 10.0f));
+        //Debug.Log("[PlayerController] - mousse position x : " + mousePosition.x + ", mousse position Z : " + mousePosition.z);
+        if (mouseInDefinedPlayZone(mousePosition.x, mousePosition.z))
         {
-            moussePosition.y = transform.position.y;
-            transform.position = moussePosition;
+            mousePosition.y = transform.position.y;
+            transform.position = mousePosition;
 
         }
     }
